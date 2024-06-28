87e96b3cab0d99d828f176c6720bbd33afbea429
oleksandrshevchuk <sasha41411@gmail.com>
2024-06-28 23:47:10 +0300

    Added app icon, deleted dark theme, small fixes

--- app/src/main/java/ua/oshevchuk/heartratetestapp/ui/MainActivity.kt
+++ app/src/main/java/ua/oshevchuk/heartratetestapp/ui/MainActivity.kt
@@ -9,18 +9,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.ui.res.stringResource
 import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
+import ua.oshevchuk.heartratetestapp.R
 import ua.oshevchuk.heartratetestapp.ui.navigation.Navigation
 import ua.oshevchuk.heartratetestapp.ui.theme.HeartRateTestAppTheme
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val cameraPermissionRequestLauncher: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            if(!isGranted){
+            if (!isGranted) {
                 Toast.makeText(
                     this,
-                    "Go to settings and enable camera permission to use this feature",
+                    getString(R.string.camera_error),
                     Toast.LENGTH_SHORT
                 ).show()
             }
@@ -33,7 +36,7 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.CAMERA
             ) == PackageManager.PERMISSION_DENIED -> {
                 cameraPermissionRequestLauncher.launch(Manifest.permission.CAMERA)
-                }
+            }
         }
     }
 
